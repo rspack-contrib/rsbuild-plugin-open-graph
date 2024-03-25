4bb4d087dd00ccf09a82e47df6ac73e33615e56b
neverland <chenjiahan.jait@bytedance.com>
2024-03-25 11:27:09 +0800

    initial implementation

--- playground/rsbuild.config.ts
+++ playground/rsbuild.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginOpenGraph } from '../src';
 
 export default defineConfig({
-  plugins: [pluginOpenGraph()],
+  plugins: [
+    pluginOpenGraph({
+      title: 'Rsbuild',
+      type: 'website',
+      url: 'https://rsbuild.dev/',
+      image: 'https://rsbuild.dev/og-image.png',
+      description: 'The Rspack-based build tool',
+      twitter: {
+        site: '@rspack_dev',
+        card: 'summary_large_image',
+      },
+    }),
+  ],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import type { RsbuildPlugin } from '@rsbuild/core';
+import type { RsbuildConfig, RsbuildPlugin } from '@rsbuild/core';
 
 export type PluginOpenGraphOptions = {
-  foo?: string;
-  bar?: boolean;
+  url?: string;
+  type?: string;
+  title?: string;
+  image?: string;
+  audio?: string;
+  video?: string;
+  locale?: string;
+  determiner?: 'a' | 'an' | 'the' | 'auto' | '';
+  description?: string;
+  twitter?: {
+    site?: string;
+    card?: string;
+    title?: string;
+    image?: string;
+    player?: string;
+    creator?: string;
+    description?: string;
+  };
 };
+
+type MetaObject = {
+  property: string;
+  content: string;
+};
+
+function getMeta(name: string, value: string): Record<string, MetaObject> {
+  return {
+    [name]: {
+      property: name,
+      content: value,
+    },
+  };
+}
 
 export const pluginOpenGraph = (
   options: PluginOpenGraphOptions = {},
@@ -11,7 +41,41 @@ export const pluginOpenGraph = (
   name: 'rsbuild-plugin-open-graph',
 
   setup(api) {
-    console.log('api', api);
-    console.log('options', options);
+    api.modifyRsbuildConfig((userConfig, { mergeRsbuildConfig }) => {
+      const meta: Record<string, MetaObject> = {};
+
+      const BASIC_KEYS = [
+        'url',
+        'type',
+        'title',
+        'image',
+        'audio',
+        'video',
+        'locale',
+        'determiner',
+        'description',
+      ] as const;
+
+      BASIC_KEYS.forEach((key) => {
+        const val = options[key];
+        if (val !== undefined) {
+          Object.assign(meta, getMeta(`og:${key}`, val));
+        }
+      });
+
+      if (options.twitter) {
+        Object.entries(options.twitter).forEach(([key, val]) => {
+          Object.assign(meta, getMeta(`twitter:${key}`, val));
+        });
+      }
+
+      const extraConfig: RsbuildConfig = {
+        html: {
+          meta,
+        },
+      };
+
+      return mergeRsbuildConfig(extraConfig, userConfig);
+    });
   },
 });
